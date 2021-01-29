0103f465a7395f3dd621de9930c68894659512fd
Fernando Patini Szmyhiel <nando.patini@gmail.com>
2021-01-29 00:20:48 +0000

    Aula 4

--- pages/_app.js
+++ pages/_app.js
@@ -34,6 +34,9 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <meta property="og:image" content={db.bg} />
+        <meta property="og:title" content="Mega Man Quiz" />
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content="Teste seus conhecimento sobre esse clássico da Capcom." />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
         {/* eslint-disable-next-line react/no-danger */}
